a699ec5bf67f8adf1844244d26cf19570081708f
Matthew Thomas <matthewthomasgb@gmail.com>
2025-11-01 14:36:58 +0000

    add lint, format, tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { mergeReports } from './methods/merge-reports';
-export { readReportsFromDirectory } from './methods/read-reports';
-export { readReportsFromGlobPattern } from './methods/read-reports';
+export { mergeReports } from './methods/merge-reports'
+export { readReportsFromDirectory } from './methods/read-reports'
+export { readReportsFromGlobPattern } from './methods/read-reports'
